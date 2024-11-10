e7d2be8199d9c17c9b0b080bb5dfba7dc2c13350
Antonymous1337 <holshouserantony@yahoo.com>
2024-11-10 15:07:20 -0800

    Initial Commit

---
+++ Pokemon Guide/Pokemon_GuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokemon_GuideApp.swift
+//  Pokemon Guide
+//
+//  Created by Antony Holshouser on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Pokemon_GuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
